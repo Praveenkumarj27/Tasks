169d48210cfe8e291dad21af7795c86bff64e7b3
praveenkumarj27 <jpraveenkumar263@gmail.com>
2023-02-02 16:52:35 +0530

    commit

--- Day 2 /script.js
+++ Day 2 /script.js
@@ -6,17 +6,63 @@
 // Note:-
 //      Values larger than MAX_VALUE are represented as Infinity and will lose their actual value.
 
-function multiply(x, y) {
-  if (x * y > Number.MAX_VALUE) {
-    return "Process as Infinity";
-  }
-  return x * y;
-}
+// Because MAX_VALUE is a static property of Number, you always use it as Number.MAX_VALUE,
+//  rather than as a property of a number value.
 
-console.log(multiply(1.7976931348623157e308, 1));
-// Expected output: 1.7976931348623157e+308
+// function multiply(x, y) {
+//   if (x * y > Number.MAX_VALUE) {
+//     return "Process as Infinity";
+//   }
+//   return x * y;
+// }
 
-console.log(multiply(1.7976931348623157e308, 2));
-// Expected output: "Process as Infinity"
+// console.log(multiply(1.7976931348623157e308, 1));
+// // Expected output: 1.7976931348623157e+308
+
+// console.log(multiply(1.7976931348623157e308, 2));
+// // Expected output: "Process as Infinity"
 
 
+// Number.MIN_SAFE_INTEGER:-
+// The Number.MIN_SAFE_INTEGER static data property represents the minimum safe integer in JavaScript, or -(253 - 1).
+// To represent integers smaller than this, consider using BigInt.
+
+
+var x = Number.MIN_SAFE_INTEGER - 1;
+var y = Number.MIN_SAFE_INTEGER - 2;
+
+console.log(Number.MIN_SAFE_INTEGER);
+// Expected output: -9007199254740991
+
+console.log(x);
+// Expected output: -9007199254740992
+
+console.log(x === y);
+// Expected output: true
+
+var z=Number.MIN_SAFE_INTEGER
+console.log("Vlaue of z "+z);
+
+
+// Number.MAX_SAFE_INTEGER:-
+// The Number.MAX_SAFE_INTEGER static data property represents the maximum safe integer in JavaScript (253 – 1).
+
+// For larger integers, consider using BigInt.
+
+var x = Number.MAX_SAFE_INTEGER + 1;
+var y = Number.MAX_SAFE_INTEGER + 2;
+
+console.log(Number.MAX_SAFE_INTEGER);
+// Expected output: 9007199254740991
+
+console.log(x);
+// Expected output: 9007199254740992
+
+console.log(x === y);
+// Expected output: true
+
+
+// execution contexts:-
+//        There are two types of execution contexts: global and function. The global execution context is created when
+// a JavaScript script first starts to run, and it represents the global scope in JavaScript. A function execution 
+// context is created whenever a function is called, representing the function's local scope.
