01cfb8bcacfee42ea1b6be9aff4f0a18028575bc
praveenkumarj27 <jpraveenkumar263@gmail.com>
2023-02-02 10:17:03 +0530

    init

---
+++ Day 1 (Feb 1)/script.js
@@ -0,0 +1,37 @@
+// Advantage of Javascript.
+
+// * Fast speed: JavaScript is executed on the client side that’s why it is very fast.
+// * Easy to learn: JavaScript is easy to learn. Any one which have basic knowledge of programming 
+// can easily lean JavaScript.
+// * Versatility: It refers to lots of skills. It can be used in a wide range of applications.
+// * Browser Compatible: JavaScript supports all modern browsers. It can execute on any browser and produce
+// same result.
+// * Server Load: JavaScript reduce the server load as it executes on the client side.
+// * Rich interfaces: JavaScript provides the drag and drop functionalities which can provides the rich look to the web pages.
+// * Popularity: JavaScript is a very popular web language because it is used every where on the web.
+// * Regular Updates: JavaScript updated annually by ECMA.
+
+
+// Disadvantage of Javascript.
+
+// * Code Visibility: JavaScript code is visible to every one and this is the biggest disadvantage of JavaScript.
+// * Stop Render: One error in JavaScript code can stop whole website to render.
+// * No Multiple Inheritance: JavaScript only support single inheritance.
+
+
+// Features of oops in javascript:-
+//    * Objects
+//    * Classes
+//    * Encapsulation
+//    * Inhertance
+
+
+// Features of Functional Programming:-
+//      JavaScript has the most important features needed for functional programming: 
+//   *First class functions: 
+//   *The ability to use functions as data values: 
+//   *pass functions as arguments, 
+//   *return functions,and assign functions to variables and object properties
+
+
+
